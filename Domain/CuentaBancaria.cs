3395c2f8e878f0c0a982bfc5e25050e44c83eb1c
Matiasahumada <pagosptcmatias@gmail.com>
2025-05-02 18:14:24 -0300

    me pase a vs code

--- Domain/CuentaBancaria.cs
+++ Domain/CuentaBancaria.cs
@@ -14,66 +14,7 @@ public abstract class CuentaBancaria
         _saldo = saldo;
         _titulares = titulares;
     }
-    #region Getters/Setters
-    public string GetNumero()
-    {
-        return _numero;
-    }
-
-    public decimal GetSaldo()
-    {
-        return _saldo;
-    }
-    public TipoCuenta GetTipo()
-    {
-        return _tipo;
-    }
-
-    public Estado GetEstado()
-    {
-        return _estado;
-    }
-
-    public void SetEstado(Estado estado)
-    {
-        _estado = estado;
-    }
-
-    public decimal GetTasaDeInteres()
-    {
-        return _tasaDeInteres;
-    }
-
-    public void SetTasaDeInteres(decimal tasaDeInteres)
-    {
-        _tasaDeInteres = tasaDeInteres;
-    }
-
-    public decimal GetLimiteDeDescubierto()
-    {
-        return _limiteDeDescubierto;
-    }
-
-    public void SetLimiteDeDescubierto(decimal limiteDeDescubierto)
-    {
-        _limiteDeDescubierto = limiteDeDescubierto;
-    }
-
-    public decimal GetComision()
-    {
-        return _comision;
-    }
-
-    public void SetComision(decimal comision)
-    {
-        _comision = comision;
-    }
-
-    public string[] GetTitulares()
-    {
-        return _titulares;
-    }
-    #endregion
+  
     public abstract void Depositar(decimal monto);
     public abstract void Retirar(decimal monto);
     public virtual void AplicarInteres() { }
