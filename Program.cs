1f56875cbccf5341118bdbf79d9c55646982de1a
ElBaral3 <agustin.barale@alu.frt.utn.edu.ar>
2025-05-02 18:47:21 -0300

    Pull Development

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Dsw2025Ej8
+﻿using Dsw2025Ej8.Domain;
+
+namespace Dsw2025Ej8
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            CuentaCorriente CC1 = new CuentaCorriente("1234", 50000, ["Titular1","Titula2"], 30,2000);
+            CuentaCorriente CC2 = new CuentaCorriente("1235", 520000, ["Titular1", "Titula2"], 20, 2300);
+            CajaAhorro CA1 = new CajaAhorro("2453", 380000, ["Titular7", "Titula8"], 40);
+            CajaAhorro CA2 = new CajaAhorro("6789", 150000, ["Titular1", "Titula2"], 15);
+            CC1.Depositar(95000);
+            CC2.Depositar(55000);
+            CA1.Depositar(86238);
+            CA2.Depositar(67812);
+            CC1.Retirar(1500000);
         }
     }
 }
